3d118df108cf781b9375417881b00679762a7173
Henrik Åhl <henrikaahl@gmail.com>
2017-11-19 14:49:24 +0000

    Changed mechanicalSpring to add spring constants for each neighbouring cell, rather than the average of both

--- src/mechanicalSpring.cc
+++ src/mechanicalSpring.cc
@@ -427,7 +427,7 @@ namespace WallMechanics {
         &indValue) 
     {  
 
-      // Do some checks on the parameters and variable indeces
+      // Do some checks on the parameters and variable indices
       if(paraValue.size() != 5) {
         std::cerr << "WallMechanics::SpringInternalExternalThreshold::"
           << "SpringInternalExternalThreshold() "
@@ -503,14 +503,18 @@ namespace WallMechanics {
           concOne = T.wall(i).cell1() != T.background() ? 
             cellData[T.wall(i).cell1()->index()][concentrationIndex] : 0;  
           concTwo = T.wall(i).cell2() != T.background() ? 
-						cellData[T.wall(i).cell2()->index()][concentrationIndex] : 0;
-          coeff   = (concOne + concTwo) / 2.0 < parameter(0) ? parameter(1) : parameter(2);
+            cellData[T.wall(i).cell2()->index()][concentrationIndex] : 0;
+          // coeff   = (concOne + concTwo) / 2.0 < parameter(0) ? parameter(1) : parameter(2);
+          coeff   = (concOne < parameter(0) ? parameter(1) : parameter(2)) + //(concOne + concTwo) / 2.0 < parameter(0) ? parameter(1) : parameter(2);
+                    (concTwo < parameter(0) ? parameter(1) : parameter(2))
         } else {
           concOne = T.wall(i).cell1() != T.background() ? 
             cellData[T.wall(i).cell1()->index()][concentrationIndex] : 0;  
           concTwo = T.wall(i).cell2() != T.background() ? 
-						cellData[T.wall(i).cell2()->index()][concentrationIndex] : 0;
-          coeff   = (concOne + concTwo) / 2.0 < parameter(0) ? parameter(3) : parameter(4);
+            cellData[T.wall(i).cell2()->index()][concentrationIndex] : 0;
+          // coeff   = (concOne + concTwo) / 2.0 < parameter(0) ? parameter(3) : parameter(4);
+          coeff   = (concOne < parameter(0) ? parameter(3) : parameter(4)) + //(concOne + concTwo) / 2.0 < parameter(0) ? parameter(1) : parameter(2);
+                    (concTwo < parameter(0) ? parameter(3) : parameter(4))
         }
 
         // If we're causing a non-physical mess, don't update.
