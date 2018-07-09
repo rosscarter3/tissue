1a16da141f86c77296d17751d533a3974ed37f58
Ross Carter <ross.carter@slcu.cam.ac.uk>
2018-07-09 14:44:25 +0100

    added more debug info to VertexFromTRBScenterTriangulationMT

--- src/mechanicalTRBS.cc
+++ src/mechanicalTRBS.cc
@@ -3523,11 +3523,11 @@ derivs(Tissue &T,
          StrainAlmansi[0][1] != StrainAlmansi[0][1] ||
          StrainAlmansi[1][0] != StrainAlmansi[1][0] ) 
         std::cerr << std::endl << "VertexFromTRBScenterTriangulationMT::derivs() WARNING!" << std::endl
-		  << "strain is wrong " << StrainAlmansi[0][0] << " " << StrainAlmansi[1][1] << " "
-		  << " " << StrainAlmansi[0][1] << " " << StrainAlmansi[1][0]
-		  << "   Q " << restingLength[0] << " " << restingLength[1] << " " << restingLength[2]
-		  << "    P " << Pa << " " << Pb << " " << Pc 
-      << " Resting Angle: "<< RestingAngle1 <<std::endl;
+		  << "StrainAlmansi is wrong " << StrainAlmansi[0][0] << " " << StrainAlmansi[1][1] << " "
+		  << " " << StrainAlmansi[0][1] << " " << StrainAlmansi[1][0] << std::endl
+		  << "Q " << restingLength[0] << " " << restingLength[1] << " " << restingLength[2] << std::endl
+		  << "P " << Pa << " " << Pb << " " << Pc << std::endl
+      << "Resting Angle: "<< RestingAngle1 <<std::endl;
       double atEa=AnisoRestLocal[0]*AnisoRestLocal[0]*Egreen[0][0]
         +AnisoRestLocal[0]*AnisoRestLocal[1]*(Egreen[0][1]+Egreen[1][0])
         +AnisoRestLocal[1]*AnisoRestLocal[1]*Egreen[1][1];
