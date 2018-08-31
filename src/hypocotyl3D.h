ad1f813353557a1478a593cae8430fb765497299
henrikjoensson <henrik.jonsson@slcu.cam.ac.uk>
2018-08-31 11:41:44 +0100

    Added Behruz version of CenterTriangulation::WallGrowth::StrainTRBS as Hypocotyl3D::StrainTRBS

--- src/hypocotyl3D.h
+++ src/hypocotyl3D.h
@@ -88,7 +88,64 @@ namespace Hypocotyl3D {
     std::vector< size_t > bottomVertices; //holds the vertex indices at the bottom
   };
 
-
+  ///
+  /// @brief This reaction is currently using ad hoc additions/changes within the code and should
+  /// only be used by an expert, i.e. Behruz? or when model file been provided by an expert.
+  ///
+  /// In a model file the reaction is defined as (parameters changed?) 
+  ///
+  /// @verbatim
+  /// Hypocotyl3D::StrainTRBS 2 3 1 1 3
+  /// k_growth s_threshold  
+  ///    
+  /// L_ij-index
+  /// InternalVarStartIndex 
+  ///
+  /// strain1_index
+  /// strain2_index
+  /// strain_vector_index
+  /// @endverbatim
+  ///
+  /// (strain value and direction calculated and updated from other (mechanical) reactions).
+  ///
+  class StrainTRBS : public BaseReaction {
+    
+  public:
+    ///
+    /// @brief Main constructor
+    ///
+    /// This is the main constructor which sets the parameters and variable
+    /// indices that defines the reaction.
+    ///
+    /// @param paraValue vector with parameters
+    ///
+    /// @param indValue vector of vectors with variable indices
+    ///
+    /// @see BaseReaction::createReaction(std::vector<double> &paraValue,...)
+    ///  
+    StrainTRBS(std::vector<double> &paraValue, 
+	       std::vector< std::vector<size_t> > 
+	       &indValue );
+    ///
+    /// @brief Derivative function for this reaction class
+    ///
+    /// @see BaseReaction::derivs(Tissue &T,...)
+    ///  
+    void derivs(Tissue &T,
+		DataMatrix &cellData,
+		DataMatrix &wallData,
+		DataMatrix &vertexData,
+		DataMatrix &cellDerivs,
+		DataMatrix &wallDerivs,
+		DataMatrix &vertexDerivs );
+    
+    void update(Tissue &T,
+		DataMatrix &cellData,
+		DataMatrix &wallData,
+		DataMatrix &vertexData,
+		double h );
+  };
+  
   ///
   /// @brief Triangular spring model for plates (2D walls) assuming
   /// triangulation with a central point on the 2D wall/cell.
