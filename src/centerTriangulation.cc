f433699d16b82f38fcb20a66a493c0d5c7b7c4c9
Henrik Jonsson <henrik.jonsson@slcu.cam.ac.uk>
2019-12-11 14:13:09 +0000

    Made sure CenterTriangulation exist in initial tissue when p2=2 given and exit if not (as it was assumed).

--- src/centerTriangulation.cc
+++ src/centerTriangulation.cc
@@ -22,16 +22,18 @@ namespace CenterTriangulation {
     // Do some checks on the parameters and variable indeces
     if( paraValue.size()!=0 && paraValue.size()!=1 && paraValue.size()!=2 ) {
       std::cerr << "CenterTriangulation::Initiate::Initiate() "
-		<< "Uses zero or one or two parameter(s). If one provided it is a flag equal to one for overriding "
-                << "If two parameters provided the second should be equal to one for initiation for two "
-                << " independent resting  lengths for neigbohr elements used for growth dependent reactions. "
+		<< "Uses zero or one or two parameter(s). "
+		<< "If one provided it is a flag equal to one for overriding "
+                << "If two parameters provided the second should be equal to one for initiation of two "
+                << "independent resting lengths for neighbour elements used for growth dependent reactions. "
 		<< "a central triangulation stored in tissue (generated from scratch instead)." 
                 << std::endl;
       exit(EXIT_FAILURE);
     }
     if( indValue.size()!=1 || indValue[0].size()!=1 ) { 
       std::cerr << "CenterTriangulation::Initiate::Initiate() "
-		<< "Start of internal cell variables index given in first level (=cell.numVariable)." << std::endl;
+		<< "Start of internal cell variables index given in first level (=cell.numVariable)."
+		<< std::endl;
       exit(EXIT_FAILURE);
     }
     
@@ -75,11 +77,15 @@ namespace CenterTriangulation {
     size_t numCell = cellData.size();
     std::vector<double> com(dimension);    
     assert (numCell==T.numCell());
+    //
+    // Default: single edge CenterTriangulation created
+    //
     if (numParameter()==0 || numParameter()==1){
+      //
+      // No centerTriangulation in init file OR existing centerTriangulation is overridden
+      //
       if (!T.cell(0).numCenterPosition() || (numParameter() && parameter(0)==1) ) {
-        
-        //No centers defined in cells in tissue or overridden (create central triangulation from scratch)
-        for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
+	for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
           size_t numInternalWall = T.cell(cellIndex).numVertex();
           cellData[cellIndex].resize(numVariable+dimension+numInternalWall);
           cellDerivs[cellIndex].resize(numVariable+dimension+numInternalWall);
@@ -100,21 +106,10 @@ namespace CenterTriangulation {
             cellData[cellIndex][numVariable+dimension+k] = distance;
             
           }
-          // sleep(1); //initiating MT dirrections randomly in xy plane
-          // srand((unsigned)time(NULL));
-        
-          // cellData[cellIndex][0]=((double)rand() / (RAND_MAX+1)) ;
-          // cellData[cellIndex][1]=((double)rand() / (RAND_MAX+1)) ;
-          // double tmp=-std::sqrt(cellData[cellIndex][0]*cellData[cellIndex][0]
-          //                      +cellData[cellIndex][1]*cellData[cellIndex][1]); 
-          
-          // cellData[cellIndex][0]/=tmp;
-          // cellData[cellIndex][1]/=tmp;
-          // std::cerr<< "MT vector"<< cellData[cellIndex][0]<<" "<< cellData[cellIndex][1]<< std::endl;
         }
       }
       else { 
-        
+	
         // Copy central position and edge length data from cells in Tissue
         for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
           size_t numInternalWall = T.cell(cellIndex).numVertex();
@@ -133,10 +128,15 @@ namespace CenterTriangulation {
         }
       }
     }
+    //
+    // Creates a centerTriangulation with double edges
+    //
     if (numParameter()==2 && parameter(1)==1){ // double resting length
+      //
+      // No centerTriangulation in init file OR existing centerTriangulation is overridden
+      //
       if (!T.cell(0).numCenterPosition() || parameter(0)==1 ) {
-        //No centers defined in cells in tissue or overridden (create central triangulation from scratch)
-        for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
+	for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {	
           size_t numInternalWall = T.cell(cellIndex).numVertex();
           cellData[cellIndex].resize(numVariable+dimension+3*numInternalWall);
           cellDerivs[cellIndex].resize(numVariable+dimension+3*numInternalWall);
@@ -185,30 +185,40 @@ namespace CenterTriangulation {
       //     for (size_t d=0; d<dimension; ++d)
       //       cellData[cellIndex][numVariable+d] = com[d];    
       //     // Set internal wall lengths to the distance btw com and the vertex
-      //     for (size_t k=0; k<2*numInternalWall; ++k) {                              // should be modified <<<<<<<<<<
+      //     for (size_t k=0; k<2*numInternalWall; ++k) { // should be modified <<<<<<<<<<
       //       cellData[cellIndex][numVariable+dimension+k] = T.cell(cellIndex).edgeLength(k);
       //     }
       //   }
       // }
     }
-    if (numParameter()==2 && parameter(1)==2){ // double resting length as continuation of  single resting length
+    //
+    // double resting length stored from original single length CenterTriangulation input
+    //
+    if (numParameter()==2 && parameter(1)==2){ 
+
       for (size_t cellIndex=0; cellIndex<numCell; ++cellIndex) {
-        
+	if (!T.cell(cellIndex).numCenterPosition()) {
+	  std::cerr << "CenterTriangulation::Initiate::initiate() ERROR: "
+		    << "When second parameter is provided with value 2, the original tissue is expected "
+		    << "to be of the form CenterTriangulation (with single edge elements). Did you "
+		    << "forget to provide -centerTri_init to simulator?" << std::endl;
+	  std::cerr << "Error triggered for cell " << cellIndex << std::endl;
+	  std::exit(EXIT_FAILURE);
+	}
         size_t numInternalWall = T.cell(cellIndex).numVertex();
         com = T.cell(cellIndex).centerPosition();
         
         cellData[cellIndex].resize(numVariable+dimension+3*numInternalWall);
         cellDerivs[cellIndex].resize(numVariable+dimension+3*numInternalWall);
-        
+	
         for (size_t k=0; k<dimension; ++k) {// center position
           cellData[cellIndex][numVariable+k] = com[k];
         }
-   
+	
         for (size_t k=0; k<numInternalWall; ++k) {// internal walls
           cellData[cellIndex][numVariable+dimension+2*k] = T.cell(cellIndex).edgeLength(k);
           cellData[cellIndex][numVariable+dimension+2*k+1] =T.cell(cellIndex).edgeLength(k); 
         }
-        
         for (size_t k=0; k<numInternalWall; ++k) { // growing component of main walls zero at initiation
           cellData[cellIndex][numVariable+dimension+2*numInternalWall+k] =0;
         }
